a9fbbd6688714e520bfaa779a0b9a0c23d2a66bb
jjwallz11 <johnnywallz86@gmail.com>
2024-11-10 19:54:37 -0600

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 // backend/app.js
-
 const express = require("express");
 require("express-async-errors");
 const morgan = require("morgan");
@@ -9,36 +8,69 @@ const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const { environment } = require("./config");
 const isProduction = environment === "production";
-const routes = require('./routes');
+const routes = require("./routes");
 const app = express();
-app.use(morgan('dev'));
+const { ValidationError } = require('sequelize');
+app.use(morgan("dev"));
 app.use(cookieParser());
 app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors());
-  }
-  
-  // helmet helps set a variety of headers to better secure your app
-  app.use(
-    helmet.crossOriginResourcePolicy({
-      policy: "cross-origin"
-    })
-  );
-  
-  // Set the _csrf token and create req.csrfToken method
-  app.use(
-    csurf({
-      cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-      }
-    })
-  );
+  // enable cors only in development
+  app.use(cors());
+}
 
-app.use(routes);// connect all the routes
+// helmet helps set a variety of headers to better secure your app
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+// Set the _csrf token and create req.csrfToken method
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+
+app.use(routes); // connect all the routes
+
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
 
 module.exports = app;
