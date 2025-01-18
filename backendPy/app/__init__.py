02801bb420ef4b550ae4320dfd1a18dd5d679118
jjwallz11 <johnnywallz86@gmail.com>
2025-01-18 10:09:11 -0600

    py refresh

--- backendPy/app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from app.config import Config
-from app import routes
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-app.register_blueprint(routes.inventory.bp)
-app.register_blueprint(routes.main.bp)
