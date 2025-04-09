8ff1c5921d867ce68886a44f5e54f7ce76879268
jjwallz11 <johnnywallz86@gmail.com>
2025-04-09 11:28:48 -0500

    destroy frontend, again

--- frontend/src/App.web.tsx
+++
@@ -1,3 +0,0 @@
-// App.web.tsx
-import App from '../App'; // uses same logic as mobile
-export default App;
--- frontend/utils/log.ts
+++
@@ -1,9 +0,0 @@
-// frontend/utils/log.ts
-
-export const logError = (err: unknown) => {
-    if (__DEV__) {
-      console.error('🔴 Error:', err);
-    }
-  
-    // Could also send to a service like Sentry, LogRocket, etc later
-  };
