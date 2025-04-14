ed6bc7b74af8410c81e566ee4330528001726e04
jjwallz11 <johnnywallz86@gmail.com>
2025-04-14 11:12:00 -0500

    locking package versions

--- frontend6/App.tsx
+++ frontend6/App.tsx
@@ -1,10 +1,11 @@
 // frontend/App.tsx
 import { ExpoRoot } from "expo-router";
-
 import { registerRootComponent } from "expo";
 
+// 👇 Correct for Expo Router v3 (not v4+)
+const ctx = require("./app");
+
 export function App() {
-  const ctx = require.context("./app");
   return <ExpoRoot context={ctx} />;
 }
 
